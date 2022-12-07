6d936a3def2fcaaabe92c31f5f7e2d8f00bb9ba4
Victoria K. Tsuha <32991496+vickats@users.noreply.github.com>
2022-12-07 18:30:28 -0300

    JS Udemys - Class 38
    Challenge

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -92,12 +92,121 @@ console.log(age3); */
 console.log(yearsUntilRetirement(1994)); */
 /*In the need to add more than just one line of code, you'll need to add the curly braces and 'return' keyword again, kepting the arrow '=>'. 'birthYear' still as parameter*/
 
-const yearsUntilRetirement = (birthYear, firstName) => {
+/* const yearsUntilRetirement = (birthYear, firstName) => {
     const age = 2022 - birthYear;
     const retirement = 60 - age;
     return `${firstName} retires in ${retirement} years 👩‍🦳`;
 }
 console.log(yearsUntilRetirement(1994, 'Victoria'));
-console.log(yearsUntilRetirement(1970, 'Bob'));
+console.log(yearsUntilRetirement(1970, 'Bob')); */
 /*Wrap the parameter with () if you need to write more parameters */
 /*It's preferred that arrow functions be used to one-line codes, but its not a rule */
+
+
+///////////////////////////////
+// 36 Functions calling other functions
+/* brief: Considering the function 'fruitProcessor' as a machine that make us juice, now we need a machine that cuts the fruits into pieces before putting them in the juice machine*/
+
+/* function cutFruitPieces(fruit){
+    return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges){
+    const applePieces = cutFruitPieces(apples);
+    const orangePieces = cutFruitPieces(oranges);
+
+    const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange`;
+    return juice; 
+}
+console.log(fruitProcessor(2, 3)); */
+
+
+///////////////////////////////
+// 37 Reviewing Functions
+
+//Let's rewrite this function to a function expression - remove the arrow and write the 'function' keyword; And call another function inside it
+/* 
+const calcAge = function (birthYear){
+    return 2037 - birthYear;
+}
+// same grammar for parameters in different functions doesn't have ANY connection or problem; they're totally different parameters (even a var outside functions w/ the same grammar)
+const yearsUntilRetirement = function (birthYear, firstName) {
+    const age = calcAge(birthYear);
+    const retirement = 65 - age;
+
+    if(retirement > 0){
+        console.log(`${firstName} retires in ${retirement} years 👩‍🦳`);
+        return retirement;//any code written after the 'return' keyword will be ignored because it immediately exit/return the function
+        console.log(`${firstName} retires in ${retirement} years 👩‍🦳`);
+    }else{
+        console.log(`${firstName} is already retired 👩‍🦳`);
+        return -1;
+    }
+    
+    //return `${firstName} retires in ${retirement} years 👩‍🦳`;
+}
+console.log(yearsUntilRetirement(1994, 'Bob'));
+console.log(yearsUntilRetirement(1970, 'Mike'));
+ */
+//The console.log does not have anything with the function itself. It's just print a message in the console. Actually, it's a fucntion itself
+
+
+///////////////////////////////
+// 38 Coding Challenge #1
+
+
+/*
+Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new gymnastics discipline, which works differently.
+Each team competes 3 times, and then the average of the 3 score is calculated (so one average score per team).
+A team ONLY wins if it has at least DOUBLE the average score of the other team. Otherwise, no team wins!
+
+1. Create an arrow function 'calcAverage' to calculate the average of 3 scores.
+2. Use the function 'checkWinner' that takes the average score of each team as parameters ('avgDolphins' and 'avgKoalas'), and then logs the winner to the console, together with the victory points, according to the rule above. Example: "Koalas win (30 vs. 13)".
+4. Use the 'checkWinner' function to determine the winner for both DATA 1 and DATA 2.
+5. Ignore draws this time.
+
+TEST DATA 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
+TEST DATA 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27 
+*/
+
+
+
+const calcAverage = totalScore => totalScore / 3;
+
+const checkWinner = function(avgDolphins, avgKoalas){
+    const scoreDolphins = calcAverage(avgDolphins);
+    const scoreKoalas = calcAverage(avgKoalas);
+
+    if (scoreDolphins >= scoreKoalas*2){
+        console.log(`Dophins wins (${scoreDolphins} xvs. ${scoreKoalas})`);
+
+    }else if(scoreKoalas >= scoreDolphins*2){
+        console.log(`Koalas wins (${scoreKoalas} xvs. ${scoreDolphins})`);
+        return;
+    }else{
+        console.log(`No one wins`);
+        return;
+    }
+}
+console.log(checkWinner(138, 168));
+
+/////////////////////////////////////////////////////
+/* const calcAge = function (birthYear){
+    return 2037 - birthYear;
+}
+
+const yearsUntilRetirement = function (birthYear, firstName) {
+    const age = calcAge(birthYear);
+    const retirement = 65 - age;
+
+    if(retirement > 0){
+        console.log(`${firstName} retires in ${retirement} years 👩‍🦳`);
+        return retirement;
+    }else{
+        console.log(`${firstName} is already retired 👩‍🦳`);
+        return -1;
+    }
+
+}
+console.log(yearsUntilRetirement(1994, 'Bob'));
+console.log(yearsUntilRetirement(1970, 'Mike')); */
