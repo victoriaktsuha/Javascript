4c515b25290ac554446b2ff3525b0b43fc31dde5
Victoria K. Tsuha <32991496+vickats@users.noreply.github.com>
2023-01-12 17:20:39 -0300

    JS Udemy #74, #75
    74 Implementing the Game Logic
    75 Manipulating CSS Styles

--- 07-Javascript in the Browser - DOM and Events Fundamentals/starter/script.js
+++ 07-Javascript in the Browser - DOM and Events Fundamentals/starter/script.js
@@ -58,11 +58,108 @@ console.log(document.querySelector('.guess').value);
 //   console.log(document.querySelector('.guess').value);
 // });
 
-document.querySelector('.check').addEventListener('click', function () {
+/* document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value); //Remembering: when we retrieve a value from a input on UI, it'll be initially a string. Depending on the objective of the code, we'll need to transform it with Number().
   console.log(guess, typeof guess);
   //we want know when if the value is valid, in other words, if its not an empty submit. Using '!guess', we meaning 'if there is no value..' and it will return a boolean value (or there is a value or there's any value) -> Obs.: '0' is a false value, meaning a 'no value'
   if (!guess) {
     document.querySelector('.message').textContent = 'No number!';
+  }
+}); */
+
+//////////////////////////////
+// 74 Implementing the Game Logic
+
+/**
+ * Implement what happens when the guess is correct;
+ * Implement what happens when the guess is too low or too high;
+ */
+
+/* 
+//To the secret number, we store in a const: Math.random() will give a random number btw 0 and 1 every you call it (through a click, console, etc); If we want a number between 1 and 20, we have to multiply Math.random by 20; And Math.trunc will remove all the decimals that Math.random() generate; And the '+1' is to reach the number 20, because since we removed the decimals, the maximum that Math.tand() will generate is 19.99999...
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+//here we set the initial score in var because it'll repeat in the 'too high' and 'too low' conditions; Initially it was just in the DOM (HMTL), but its a very good practice to store data in the code
+let score = 20;
+//To see if this logic is running, we'll display it in the main box as its own text and every time we reload the page, we'll should see a different number:
+document.querySelector('.number').textContent = secretNumber;
+// The click event doesn't reload the page
+document.querySelector('.check').addEventListener('click', function () {
+  //Remembering: when we retrieve a value from a input on UI, it'll be initially a string. Depending on the objective of the code, we'll need to transform it with Number().
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+  //we want know when if the value is valid, in other words, if its not an empty submit. Using '!guess', we meaning 'if there is no value..' and it will return a boolean value (or there is a value or there's any value) -> Obs.: '0' is a false value, meaning a 'no value'
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+    document.querySelector('body').style.backgroundColor = 'green';
+  } else if (guess > secretNumber) {
+    //We added another condition to prevent the score going down after reaching 0; When it happens, the user actually lost the game and nothing else must happen
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too high!';
+      score--; //(score-- => score = score - 1)
+      document.querySelector('.score').textContent = score; //here we update the text with the current score; It NEED to be in that order: first the 'calculation' (score--) and then the text. Otherwise, the text will be out of date
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+      document.querySelector('body').style.backgroundColor = 'darkred';
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+      document.querySelector('body').style.backgroundColor = 'darkred';
+    }
+  }
+});
+ */
+
+//////////////////////////////
+// 75 Manipulating CSS Styles
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  // When there is no input
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number!';
+
+    //When player wins
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    // When guess is too high
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+      //document.querySelector('body').style.backgroundColor = 'darkred';
+    }
+
+    // When guess is too low
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+      //document.querySelector('body').style.backgroundColor = 'darkred';
+    }
   }
 });
