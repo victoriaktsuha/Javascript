d2b87bffbd18831dcf4450f32b93e5c058f62b50
vickats <32991496+vickats@users.noreply.github.com>
2023-02-06 18:34:57 -0300

    JS Udemy - #90

--- 10-Behind-the-Scenes/starter/script.js
+++ 10-Behind-the-Scenes/starter/script.js
@@ -15,3 +15,27 @@
  * => Single-threaded: & Non-blocking event loop: Let's strat with concurrency model: its how the JS engine handles multipe tasks happening at the same time. And why do we need that ? Because the JS runs in one single thread, so it can only do one thing at a time. In computing, a thread is like a set of instructions that runs in CPU. so basically, thread is where our code is actually executed in a machines processor. So what about a long-running task, like fetching data from a remote serve ? Sounds like it would block the single thread. However, we want a non-blocking behavior. And how do we achieve that ? Ny using an event loop: takes long running tasks, executes them in the "backgorund", and puts them back in the main thread once they're finished
 
  */
+
+/////////////////
+// 90 The JavaScript Engine and Runtime
+
+/**JS Engine is a program that exuecutes JS codes
+ * Every browser has its ow JS engine, but the most popular is the V8 from Google (node.js do the same as V8 - serve-side service)
+ * Any JS engine always contains a 'call stack' and a 'heap'
+ * => Call Stack: is where our code is actually executed using something call 'execution context'
+ * => Heap: is a unstructured memory pool that stores all the objects that our application needs
+ * So how the code is compiled to machine code so it can be executed afterwards ?
+ * First, a computer science sidenote: difference between compilation and interpretation
+ * => Compilation: Entire source code is converted into machine code at once, and written to a binary file that can be executed by a computer
+ * => Interpretation: Interpreter runs through the source code and executes it line by line; The code is read and executed at the same time
+ * JS use to be a interpreted language, but with modern JS and web app fully devepeloped, low performance is not longer acceptable
+ * Now JS use => JIT (Just-in-time) compilation: Entire code is converted into machine code at once, then executed immediately, a mix from compilation and interpretation, removing the binary/portable file before the execution
+ * How JS Engine works: 1- parse (read) the code, tranforming it into a AST (Abstract syntax tree), diving and identifying the language, terms, keywords, etc. -> 2- compilation into machine code -> 3- Execution immediately (happening in call stack); But it doesn't ends here: the engine starts execution a non-optimized code just to start as fast as possible and in the background, this code is being optimized and recompiled during the program execution, turning engines as V8 so fast
+ * All this process happens inside some special threads inside the engine that we cannot access from our code
+ * JS runtime: we can say that its like a container including all the things that we need to use JS, in this case, in the browser
+ * To make it happen, we need beside the engine, the web API's (functionalities that are 'lean' to our JS code, because they're not from JS) through the global window object; And the callback queue, a data structure that contains all the callback queue funtions that are ready to be executed, like event listener (click). What happens after the event happen is that the callback funtion is put on this callback queue and as soon the call stack is empty, this function is moved to the call stack to be executed, it happens because the 'event loop' (essential for non-blocking concurrency model)
+ * The difference between the JS runtime in the browser and in the node.js, is that in node.js we don't have the web API's because its the browser that provide it; Instead, we have the multiples C++ bindings and thread pool
+ */
+
+///////////////////////////
+// 91 Execution Contexts and The Call Stack
