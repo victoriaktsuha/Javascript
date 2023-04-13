19295f4bb1790a12e581971edb49e179f0d0564f
vickats <32991496+vickats@users.noreply.github.com>
2023-04-13 14:45:36 -0300

    JS Udemy #125 String Methods Practice

--- 11-Data-Structures-Operators/starter/script.js
+++ 11-Data-Structures-Operators/starter/script.js
@@ -1388,7 +1388,7 @@ Afterwards, test with your own test data!
 
 GOOD LUCK 😀
 */
-
+/* 
 document.body.append(document.createElement('textarea'));
 document.body.append(document.createElement('button'));
 
@@ -1406,3 +1406,35 @@ document.querySelector('button').addEventListener('click', function () {
     console.log(`${output.padEnd(20)}${'✅'.repeat(i + 1)}`);
   }
 });
+ */
+
+////////////////////
+// 125 String Methods Practice
+
+console.log(flights.split('+')); // >(4) ['_Delayed_Departure;fao93766109;txl2133758440;11:25', '_Arrival;bru0943384722;fao93766109;11:45', '_Delayed_Arrival;hel7439299980;fao93766109;12:05', '_Departure;fao93766109;lis2323639855;12:30']
+
+const getCode = str => str.slice(0, 3).toUpperCase();
+
+for (const flight of flights.split('+')) {
+  //console.log(flight);
+  // _Delayed_Departure;fao93766109;txl2133758440;11:25
+  // _Arrival;bru0943384722;fao93766109;11:45
+  // _Delayed_Arrival;hel7439299980;fao93766109;12:05
+  // _Departure;fao93766109;lis2323639855;12:30
+
+  //console.log(flight.split(';'));
+  // >(4) ['_Delayed_Departure', 'fao93766109', 'txl2133758440', '11:25']
+  // >(4) ['_Arrival', 'bru0943384722', 'fao93766109', '11:45']
+  // >(4) ['_Delayed_Arrival', 'hel7439299980', 'fao93766109', '12:05']
+  // >(4) ['_Departure', 'fao93766109', 'lis2323639855', '12:30']
+
+  const [type, from, to, time] = flight.split(';');
+  const output = `${type.startsWith('_Delayed') ? '🔴' : ''}${type.replaceAll(
+    '_',
+    ' '
+  )} from ${getCode(from)} to ${getCode(to)} (${time.replace(
+    ':',
+    'h'
+  )})`.padStart(50);
+  console.log(output);
+}
