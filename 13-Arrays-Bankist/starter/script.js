8cd3df0bf336c9c87f5f7cd075634c0123e4fb46
vickats <victoriaktsuha@outlook.com>
2023-05-18 13:14:43 -0300

    JS Udemy - #148 e 149

--- 13-Arrays-Bankist/starter/script.js
+++ 13-Arrays-Bankist/starter/script.js
@@ -288,3 +288,50 @@ const displayMovements = function (movements) {
 };
 
 displayMovements(account1.movements);
+
+///////////////////////////
+// 148 Coding Challenge #1
+
+/*
+Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners about ther dog's age, and sotred the data into an array (one array for each).
+Dor now, ther are just interested in knowing whether a dog is an adult or a puppy. A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years old.
+
+Create a function 'checkDogs', which accepts 2 arrays of dog's age ('dogsJulia' and 'dogsKate'), and does the following things:
+
+1. Julia found out that the owners of the FIRST and the LAST TWO dogs actually have cats, not dogs! So create a shallow copy of Julia's array, and remove the cat ages from that copied array (because it's a bad practice to mutate function parameters)
+2. Create an array with both Julia's (corrected) and Kate's data
+3. For each remaining dog, log to console whether it's an adult ("Dog number 1 is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy 🐶")
+4. Run the function for both test datasets
+
+HINT: Use tools from all lectures in this section so far
+
+TEST DATA 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
+TEST DATA 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
+ */
+
+const checkDogs = function (dogsJulia, dogsKate) {
+  const dogsJuliaCorrected = dogsJulia.slice(); //To JUST create a copy
+  dogsJuliaCorrected.splice(0, 1); // Remove the copy first element
+  dogsJuliaCorrected.splice(-2); // Remove the copy last two element
+  const dogs = dogsJuliaCorrected.concat(dogsKate); // Concat all data
+  dogs.forEach(function (age, i) {
+    if (age > 3) {
+      console.log(`Dog number ${i + 1} is an adult, and is ${age} years old`);
+    } else {
+      console.log(`Dog number ${i + 1} is still a puppy 🐶`);
+    }
+  });
+};
+
+checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
+
+///////////////////////////
+// 149 Data Transformations: map, filter, reduce (array methods)
+
+// MAP - Similar to the 'forEach' method, but it creates a brand new array based on the original array; It lopps over an array an in each iteration it applies a callback function, that we specify on our code, to the current array element and stores the result into a new array, this method 'maps' the original array to a new one
+// FILTER - Filters elements in the original array which satisfy a certain condition, returning a new array containing the array elements that passed a specified test condition (true)
+// REDUCE - It boil ("reduce") all array elements down to one single value (e.g. adding all elements together). For example of adding up all numbers in the array, we need to specify an operation like this one where we have an 'accumulator' variable. Then as the reduce method loops over the array, it keeps adding the current element onto the accumulator until ate the end oof the loop we have the total sum of all elements, like a snowball that gets bigger as it rolls down a hill.
+
+///////////////////////////
+// 150 The Map Method
